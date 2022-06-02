a74e73dae32348db1a28f35bff457937a3916874
Abhishek Thakur <72880760+AbhishekThak344@users.noreply.github.com>
2022-06-03 00:39:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Merkle from './components/merkle'
+
+export default function Home() {
+  return (
+<>
+
+<Merkle />
+</>    
+
+    )
+}
